a627ff678e62eb54eb403ec0cc5cdf904d738afb
Zwazel <zwazel@hotmail.com>
2022-02-25 00:10:44 +0100

    ehehehe unit wird dargstellt!

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Sprite, Stage} from '@inlet/react-pixi'
+import myFirstUnitImage from './assets/img/units/my_first_unit/goodSoupMobil.png'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+            <Stage>
+                <Sprite image={myFirstUnitImage} x={100} y={100}/>
+            </Stage>
+        </div>
+    );
 }
 
 export default App;
