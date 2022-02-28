c61739d16ae39c4e821bdf29e2f0b538ea939a0d
Zwazel <zwazel@hotmail.com>
2022-02-28 08:16:13 +0100

    EHEEEEY DISPLAYING UNITS!

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,8 +1,49 @@
 import User from "./classes/User";
-import {useState} from "react";
+import {useEffect, useState} from "react";
+
+interface Units {
+    units: Unit[];
+}
+
+interface Unit {
+    id: number;
+    name: string;
+    level: number;
+    unitType: string;
+    customNamesAllowed: boolean;
+}
 
 export default function Profile(props: { user: User }) {
     const [user] = useState(props.user);
+    const [units, setUnits] = useState<Units>({units: []});
+
+    useEffect(() => {
+        fetch(`${process.env.REACT_APP_FETCH_CALL_DOMAIN}/authenticated/user/getAllUnits`, {
+            method: "GET",
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            credentials: 'include',
+        }).then(r => {
+            return r.json()
+        }).then(r => {
+            console.log("response", r);
+            let units: Unit[] = [];
+            for (let unit of r) {
+                units.push({
+                    id: unit.id,
+                    name: unit.name,
+                    level: unit.level,
+                    unitType: unit.unitType,
+                    customNamesAllowed: unit.customNamesAllowed,
+                });
+            }
+            setUnits({units: units});
+        });
+    }, []);
+
+    console.log("units", units);
 
     return (
         <div>
@@ -10,6 +51,28 @@ export default function Profile(props: { user: User }) {
             <p>
                 Your id: {user.id}
             </p>
+            {/*display the units*/}
+            <h2>Your units</h2>
+            <table>
+                <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Type</th>
+                    <th>Level</th>
+                </tr>
+                </thead>
+                <tbody>
+                {units.units.map(unit => {
+                    return (
+                        <tr key={unit.id}>
+                            <td>{unit.name}</td>
+                            <td>{unit.unitType}</td>
+                            <td>{unit.level}</td>
+                        </tr>
+                    )
+                })}
+                </tbody>
+            </table>
         </div>
     );
 }
