6f0070bcd56ad7777671c75f025b78f0bcfe4bd8
Zwazel <zwazel@hotmail.com>
2022-02-28 20:24:26 +0100

    little extra text

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -3,8 +3,10 @@ import "../assets/css/loading.css";
 
 export default function Loader() {
     return (
-        <div>
-            <h1 className="loader">Loading...</h1>
+        <div className="loader">
+            <h1>Loading...</h1>
+            {/*todo: show this text only after a certain amount of time!!!!*/}
+            <p>If it's taking long, it's probably Heroku waking up...</p>
         </div>
     );
 }
