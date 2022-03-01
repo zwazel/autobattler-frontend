36c6de8c034b28ea9311989c881e7afd985ef093
Zwazel <zwazel@hotmail.com>
2022-03-01 07:53:43 +0100

    BUILD MAL ENDLICH

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -4,25 +4,27 @@ import "../assets/css/loading.css";
 export default function Loader() {
     const [count, setCount] = React.useState(0);
     const waitTime = 10; // seconds to wait before showing the extra text
-    const timer = () => {
-        setCount(count + 1);
-    }
 
     useEffect(
         () => {
+            const timer = () => {
+                setCount(count + 1);
+            }
+
             if (count >= waitTime) {
                 return;
             }
             const id = setInterval(timer, 1000);
             return () => clearInterval(id);
         },
-        [count, timer]
+        [count]
     );
 
     return (
         <div>
             <h1 className="loader">Loading...</h1>
-            {count >= waitTime && <span className={"info"}>If it's taking long, it's probably Heroku waking up...</span>}
+            {count >= waitTime &&
+                <span className={"info"}>If it's taking long, it's probably Heroku waking up...</span>}
         </div>
     );
 }
