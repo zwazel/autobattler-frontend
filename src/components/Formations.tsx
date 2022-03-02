7f858a7172f961dbe6a5905a5a329c3b815fddc7
Zwazel <zwazel@hotmail.com>
2022-03-02 20:36:08 +0100

    ich chan units ume dragge :D

--- src/components/Formations.tsx
+++ src/components/Formations.tsx
@@ -1,4 +1,5 @@
-import {useEffect} from "react";
+import React, {useEffect} from "react";
+import PIXI from "pixi.js";
 import {Sprite, Stage} from '@inlet/react-pixi'
 import myFirstUnitImage from '../assets/img/units/my_first_unit/goodSoupMobil.png'
 import {UnitType} from "./Navigation";
@@ -12,9 +13,37 @@ import {UnitType} from "./Navigation";
 //     unit: Unit;
 // }
 
+interface Draggable extends PIXI.DisplayObject {
+    data: PIXI.InteractionData | null;
+    dragging: boolean;
+}
+
 export default function Formations(props: { unitTypes: UnitType[] }) {
     // const unitTypes = props.unitTypes;
     // const [formations, setFormations] = useState<Formation[]>([]);
+
+    const onDragStart = (event: PIXI.InteractionEvent) => {
+        const sprite = event.currentTarget as Draggable;
+        sprite.alpha = 0.5;
+        sprite.data = event.data;
+        sprite.dragging = true;
+    };
+
+    const onDragEnd = (event: PIXI.InteractionEvent) => {
+        const sprite = event.currentTarget as Draggable;
+        sprite.alpha = 1;
+        sprite.dragging = false;
+        sprite.data = null;
+    };
+
+    const onDragMove = (event: PIXI.InteractionEvent) => {
+        const sprite = event.currentTarget as Draggable;
+        if (sprite.dragging) {
+            const newPosition = sprite.data!.getLocalPosition(sprite.parent);
+            sprite.x = newPosition.x;
+            sprite.y = newPosition.y;
+        }
+    };
 
     useEffect(() => {
         fetch(`${process.env.REACT_APP_FETCH_CALL_DOMAIN}/authenticated/user/getAllFormations`, {
@@ -38,8 +67,20 @@ export default function Formations(props: { unitTypes: UnitType[] }) {
     return (
         <>
             <h1>Formations</h1>
-            <Stage>
-                <Sprite image={myFirstUnitImage} x={100} y={100}/>
+            <Stage width={500} height={500} options={{
+                backgroundColor: 0x4287f5,
+            }}>
+                <Sprite
+                    image={myFirstUnitImage}
+                    x={100} y={100}
+                    anchor={0.5}
+                    interactive
+                    buttonMode
+                    pointerdown={onDragStart}
+                    pointerup={onDragEnd}
+                    pointerupoutside={onDragEnd}
+                    pointermove={onDragMove}
+                />
             </Stage>
         </>
     )
